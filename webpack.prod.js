462ff2e46972d45f147ade6df74c687efc170953
Raphael Dozzi <rdozzi84@gmail.com>
2020-11-20 20:44:10 -0800

    feat: Created and filled webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                  'file-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+    ]
+}
