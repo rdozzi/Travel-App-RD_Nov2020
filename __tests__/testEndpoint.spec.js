c388822bd5e705e6b7a0d8aadd2f2d5ac8b3ff0c
Raphael Dozzi <rdozzi84@gmail.com>
2020-12-04 22:12:42 -0800

    feat: Updated testEndpoint.spec but still not functional

--- __tests__/testEndpoint.spec.js
+++ __tests__/testEndpoint.spec.js
@@ -1,11 +1,14 @@
+import 'regenerator-runtime/runtime'
+
 const app = require('../src/server/server.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
 describe('Test Endpoint Response', () => {
-    test('Obtain 200 server response from call', async () => {
+    test('Obtain 200 server response from call', async done => {
         
         const response = await request.get('/getGeonames');
         expect(response.status).toBe(200);
+        done();
     })
 })
